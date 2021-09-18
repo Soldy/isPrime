f8b92c65a1a10eb2213a74cb5e8c7263a00c0e57
Soldy <4786022+Soldy@users.noreply.github.com>
2021-09-18 22:43:39 +0100

    in python
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ isPrime.py
@@ -0,0 +1,12 @@
+primes = 1
+
+def isPrime(number):
+    for i in range(2, number):
+        if(number % i == 0):
+            return False
+    return True
+
+for k in range(3, 2000000):
+    if isPrime(k):
+        primes += 1
+print (primes)
