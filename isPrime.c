4464ab4fc8d055c94e9161b7359d2faa537c6b45
Soldy <4786022+Soldy@users.noreply.github.com>
2021-09-19 13:44:52 +0100

    one step closer to the other
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- isPrime.c
+++ isPrime.c
@@ -4,7 +4,7 @@ int primes =0;
 
 bool isPrime(int number){
    int i;
-   for (i = 2; i < number; i++ ){
+   for (i = 2; i < (number / 2 ); i++ ){
        if (number % i == 0){
            return false;
        }
@@ -14,7 +14,7 @@ bool isPrime(int number){
 
 int main(){
     int k;
-    for (k = 3; k < 200000; k++){
+    for (k = 3; k < 2000000; k++){
         if (isPrime(k) == true){
            primes++;
         }
